07c02769a0910e45d93ae64732116ef029279694
mdshohagmia <shohaghassan14@hotmail.com>
2018-10-07 22:14:39 +0600

    Added Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { DepartmentComponent } from './department/department.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'studentList' },
+  { path: 'studentList', component: StudentListComponent },
+  { path: 'student/:id', component: StudentComponent },
+  { path: 'departmentList', component: DepartmentListComponent },
+  { path: 'department/:departmentId', component: DepartmentComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { DepartmentComponent } from './department/department.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    DepartmentComponent,
+    PageNotFoundComponent,
+    StudentListComponent,
+    DepartmentListComponent
   ],
   imports: [
     BrowserModule,
